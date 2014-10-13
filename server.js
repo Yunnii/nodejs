2d043fe7261f4cd1fd2650f8719bc18eea6dc8fe
Danil Mironov <danmir@me.com>
2014-10-13 23:32:56 +0600

    "Access-Control-Allow-Origin", "*"

--- server.js
+++ server.js
@@ -6,14 +6,18 @@ var is_tram_comming = 0
 setInterval(function () {
 	is_tram_comming = (is_tram_comming += 1) % 2
     console.log("Changing tram state to: ", is_tram_comming);
-}, 7000);
+}, 20000);
 
 app.get('/', function(req, res){
-  res.send('The state of tram is: ' + is_tram_comming);
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.send('The state of tram is: ' + is_tram_comming);
 });
 
 app.get('/tram_api', function(req, res){
-  res.json({ is_tram_comming: is_tram_comming })
+	res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json({ is_tram_comming: is_tram_comming, 
+  	           "permissions": ["http://www.google.com/", "http://*/"], 
+  	         })
 });
 
 var server = app.listen(3000, function() {
